cfd4a00c2d3308c9ebd286061e965d91c86b3b5d
Leon Cruz <leon.cruz.teixeira@gmail.com>
2025-08-06 19:58:06 -0300

    Logic for frame touch and overlap

--- app/models/frame.rb
+++ app/models/frame.rb
@@ -1,2 +1,39 @@
 class Frame < ApplicationRecord
+  validates :center_x, :center_y, :width, :height, presence: true
+  validates :width, :height, numericality: { greater_than: 0 }
+  validate :does_not_touch_or_overlap_other_frames
+
+  def left
+    center_x - width / 2
+  end
+
+  def right
+    center_x + width / 2
+  end
+
+  def top
+    center_y + height / 2
+  end
+
+  def bottom
+    center_y - height / 2
+  end
+
+  private
+
+  def does_not_touch_or_overlap_other_frames
+    Frame.where.not(id: id).find_each do |other|
+      if overlaps_or_touches?(other)
+        errors.add(:base, "Frame touches or overlaps another frame (ID=#{other.id})")
+        break
+      end
+    end
+  end
+
+  def overlaps_or_touches?(other)
+    !(right < other.left ||
+      left > other.right ||
+      top < other.bottom ||
+      bottom > other.top)
+  end
 end
---
+++ spec/models/frame_spec.rb
@@ -0,0 +1,36 @@
+require "rails_helper"
+
+RSpec.describe Frame, type: :model do
+  describe "validations" do
+    it { is_expected.to validate_presence_of(:center_x) }
+    it { is_expected.to validate_presence_of(:center_y) }
+    it { is_expected.to validate_presence_of(:width) }
+    it { is_expected.to validate_presence_of(:height) }
+
+    it { is_expected.to validate_numericality_of(:width).is_greater_than(0) }
+    it { is_expected.to validate_numericality_of(:height).is_greater_than(0) }
+
+    describe "#does_not_touch_or_overlap_other_frames" do
+      let!(:existing_frame) do
+        described_class.create!(center_x: 100, center_y: 100, width: 50, height: 30)
+      end
+
+      it "is invalid if it touches another frame on the right" do
+        touching_frame = described_class.new(center_x: 150, center_y: 100, width: 50, height: 30)
+        expect(touching_frame).not_to be_valid
+        expect(touching_frame.errors[:base]).to include(a_string_including("Frame touches or overlaps another frame"))
+      end
+
+      it "is invalid if it overlaps another frame" do
+        overlapping_frame = described_class.new(center_x: 110, center_y: 100, width: 50, height: 30)
+        expect(overlapping_frame).not_to be_valid
+        expect(overlapping_frame.errors[:base]).to include(a_string_including("Frame touches or overlaps another frame"))
+      end
+
+      it "is valid if it is clearly separated from another frame" do
+        valid_frame = described_class.new(center_x: 200, center_y: 100, width: 50, height: 30)
+        expect(valid_frame).to be_valid
+      end
+    end
+  end
+end
